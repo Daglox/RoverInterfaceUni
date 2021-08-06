ebb300df5676a16127dd6ab9606730c575e9af76
Tavi <tavidanner96@gmail.com>
2021-08-06 12:08:03 -0500

    Set constant -> ACCESS_TOKEN

--- app/src/main/java/com/daglox/myapplication/RestApi/ConstantsRestApi.java
+++ app/src/main/java/com/daglox/myapplication/RestApi/ConstantsRestApi.java
@@ -2,7 +2,7 @@ package com.daglox.myapplication.RestApi;
 
 public final class ConstantsRestApi {
     public static final String ROOT_URL = "https://apim-rover-fa-dev.azure-api.net/";
-    public static final String ACCESS_TOKEN = "";
+    public static final String ACCESS_TOKEN = "471d2acb790f4f55bab1c87ec87768bd";
 
     public static final String KEY_API_INFO_ENVIRONMENT = "";
     public static final String URL_API_INFO_ENVIRONMENT = ROOT_URL + KEY_API_INFO_ENVIRONMENT;
