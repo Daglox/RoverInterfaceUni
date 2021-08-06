f9256732240f8d64b31fe68a5b60b023c3b9ecc2
Tavi <tavidanner96@gmail.com>
2021-08-06 00:23:22 -0500

    Update HomeFragment view

--- app/src/main/java/com/daglox/myapplication/Fragments/HomeFragment.java
+++ app/src/main/java/com/daglox/myapplication/Fragments/HomeFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daglox.myapplication.POJO.EnvironmentItem;
@@ -18,22 +19,23 @@ import com.daglox.myapplication.R;
 
 public class HomeFragment extends Fragment implements IHomeFragment{
 
-    private EditText edtNumber;
     private Button btnRefresh;
+    private TextView tvTemperatureInfo;
+    private TextView tvHumidityInfo;
     private IHomeFragmentPresenter iHomeFragmentPresenter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         iHomeFragmentPresenter = new HomeFragmentPresenter(this, getContext());
-        edtNumber = v.findViewById(R.id.edtNumber);
         btnRefresh = v.findViewById(R.id.btnRefresh);
         return v;
     }
 
     @Override
     public void showEnvironmentInfo(EnvironmentItem environmentItem) {
-
+        tvTemperatureInfo.setText(environmentItem.getTemperature());
+        tvHumidityInfo.setText(environmentItem.getHumidity());
     }
 
 }
--- app/src/main/java/com/daglox/myapplication/Presenter/HomeFragmentPresenter.java
+++ app/src/main/java/com/daglox/myapplication/Presenter/HomeFragmentPresenter.java
@@ -1,9 +1,20 @@
 package com.daglox.myapplication.Presenter;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.daglox.myapplication.Fragments.IHomeFragment;
+import com.daglox.myapplication.Model.EnvironmentResponse;
 import com.daglox.myapplication.POJO.EnvironmentItem;
+import com.daglox.myapplication.RestApi.Adapter.RestApiAdapter;
+import com.daglox.myapplication.RestApi.ConstantsRestApi;
+import com.daglox.myapplication.RestApi.EndPointsAPI;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragmentPresenter implements IHomeFragmentPresenter {
 
@@ -18,6 +29,23 @@ public class HomeFragmentPresenter implements IHomeFragmentPresenter {
 
     @Override
     public void getEnvironmentInfo() {
+        RestApiAdapter restApiAdapter = new RestApiAdapter();
+        Gson gsonEnvironmentInfo = restApiAdapter.buildGsonDeserializeEnvironmentInfo();
+        EndPointsAPI endPointsAPI = restApiAdapter.stablishConnectionRestApiAzure(gsonEnvironmentInfo);
+        Call<EnvironmentResponse> environmentResponseCall = endPointsAPI.getRecentEnvironment("Bearer" + ConstantsRestApi.ACCESS_TOKEN);
+        environmentResponseCall.enqueue(new Callback<EnvironmentResponse>() {
+            @Override
+            public void onResponse(Call<EnvironmentResponse> call, Response<EnvironmentResponse> response) {
+                EnvironmentResponse environmentResponse = response.body();
+                environmentItem = environmentResponse.getEnvironmentItem();
+                iHomeFragment.showEnvironmentInfo(environmentItem);
+            }
 
+            @Override
+            public void onFailure(Call<EnvironmentResponse> call, Throwable t) {
+                Toast.makeText(context, "Falló la conexión con servidor", Toast.LENGTH_LONG).show();
+                Log.e("Connection failed", t.toString());
+            }
+        });
     }
 }
