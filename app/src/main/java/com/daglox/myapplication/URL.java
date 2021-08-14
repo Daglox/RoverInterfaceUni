1c92300f66d24b3d00e8caf220e4281f1ef349b2
Tavi <tavidanner96@gmail.com>
2021-08-14 16:48:28 -0500

    Update api getAccess in URL class

--- app/src/main/java/com/daglox/myapplication/URL.java
+++ app/src/main/java/com/daglox/myapplication/URL.java
@@ -1,7 +1,7 @@
 package com.daglox.myapplication;
 
 public final class URL {
-    public static final String URL_LOGIN = "https://apim-rover-fa-dev.azure-api.net/FUNCTION-ROVER-FA-ISABEL-DEV/getAccess";
+    public static final String URL_LOGIN = "https://apim-rover-fa-dev.azure-api.net/FUNCTION-ROVER-FA-ISABEL-DEV-PE/getAccess";
     public static final String URL_VERIFY="https://apim-rover-dev-pe.azure-api.net/FUNCTION-ROVER-FA-ISABEL-DEV-PE/verifyEmail";
     public static final String URL_UPDATE="https://apim-rover-dev-pe.azure-api.net/FUNCTION-ROVER-FA-ISABEL-DEV-PE/updatePassword";
     public static final String URL_KEY="471d2acb790f4f55bab1c87ec87768bd";
